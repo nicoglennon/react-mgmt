1f98127221bb60b39186d57f8292cf0b509b2a1c
Nico Glennon <nicoglennon@icloud.com>
2019-09-14 23:10:46 -0500

    prettier formatting

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React, { useContext, useReducer } from "react";
+import React, { useContext, useReducer } from 'react'
 
-export const MgmtContext = React.createContext();
+export const MgmtContext = React.createContext()
 export const MgmtProvider = ({ reducer, initialState, children }) => (
   <MgmtContext.Provider value={useReducer(reducer, initialState)}>
     {children}
   </MgmtContext.Provider>
-);
-export const useMgmt = () => useContext(MgmtContext);
+)
+export const useMgmt = () => useContext(MgmtContext)
+export const MgmtConsumer = MgmtContext.Consumer
